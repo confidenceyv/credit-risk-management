3a5b99ca74d3fa563c967d82958cd7b093dcd9a6
confidenceyv <125979175+confidenceyv@users.noreply.github.com>
2023-08-09 15:40:51 +0100

    Update Data Summarization - Transactions (1).py

--- Corporate Credit Score/Data Summarization - Transactions (1).py
+++ Corporate Credit Score/Data Summarization - Transactions (1).py
@@ -6,13 +6,10 @@ spark = SparkSession.builder.getOrCreate()
 # COMMAND ----------
 
 # Enable storage as a temporary directory
-storage_account_name = "ubariskdl"
-
-storage_account_key = "WIltzoJWmkC9x1pmXadJggmodCMFHU5A8JgS8eoInZkaoNZ9acraHrkALVOQgjtYCHgcHiQbTPhyO1DK1slrew=="
-storage_container_name = "staging"
-
-temp_dir_url =  "wasbs://staging@ubariskdl.blob.core.windows.net/".format(storage_container_name, storage_account_name)
-
+storage_account_name = "xxxxxx"
+storage_account_key = "xxxxxx=="
+storage_container_name = "dbtemp"
+temp_dir_url =  "wasbs://xxxxxx.blob.core.windows.net/".format(storage_container_name, storage_account_name)
 spark_config_key = "fs.azure.account.key.ubariskdl.blob.core.windows.net".format(storage_account_name)
 spark_config_value = storage_account_key
 
@@ -21,12 +18,11 @@ spark.conf.set(spark_config_key, spark_config_value)
 # COMMAND ----------
 
 # connect to SQL datawarehouse
-servername = "ubariskdbsvr.database.windows.net"
-databasename = "biadmin02"
-password = "sqlAdmin123#"
-
-
-sql_dw_connection_string = "jdbc:sqlserver://ubariskdbsvr.database.windows.net:1433;database=ubariskmldb;user=biadmin02@ubariskdbsvr;password=sqlAdmin123#;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;".format(servername, databasename, servername, password)
+servername = "xxxxxx"
+databasename = "xxxxxx"
+password = "xxxxxx#"
+user = "xxxxxx"
+sql_dw_connection_string = "jdbc:sqlserver://xxxxxx.database.windows.net:1433;database=xxxxxx;user=xxxxxx;password=xxxxxx#;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;".format(servername, databasename, servername, password,user)
 
 # COMMAND ----------
 
